d4807fbd583266ceef6277706a5747999879bfda
oagutu <agutu@obolas-MacBook-Pro.local>
2018-08-16 21:09:36 +0300

    [#159356728]Modify error messages for invalid workout durations

--- wger/manager/views/schedule_step.py
+++ wger/manager/views/schedule_step.py
@@ -72,13 +72,18 @@ class StepCreateView(WgerFormMixin, CreateView, PermissionRequiredMixin):
                 sum_duration = sum_duration['duration__sum'] or 0
                 derived_duration = duration + sum_duration
 
+                error_msg1 = 'Invalid duration - cycle duration exceeding '
+                error_msg2 = ' weeks left in cycle'
                 # Check if duration exceeds cycle duration
                 if schedule.period == 'Macrocycle' and derived_duration > 52:
-                    raise ValidationError(_('Invalid duration- cycle duration exceeding 1 year'))
+                    msg = error_msg1 + '1 year' + '.' + str(52-sum_duration) + error_msg2
+                    raise ValidationError(_(msg))
                 elif schedule.period == 'Mesocycle' and derived_duration > 6:
-                    raise ValidationError(_('Invalid duration- cycle duration exceeding 6 weeks'))
+                    msg = error_msg1 + '6 weeks' + '.' + str(6-sum_duration) + error_msg2
+                    raise ValidationError(_(msg))
                 elif schedule.period == 'Microcycle' and derived_duration > 1:
-                    raise ValidationError(_('Invalid duration- cycle duration exceeding 1 week'))
+                    raise ValidationError(_(
+                        'Workout already exists. Delete existing workout to add new one'))
 
                 return duration
 
